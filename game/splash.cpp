5968dcdee0389b10c40b23401d22bd2b4a363e73
John Chadwick <john@jchw.io>
2020-07-05 13:05:47 -0700

    Add WSplash implementation.

---
+++ game/splash.cpp
@@ -0,0 +1,206 @@
+﻿#include "splash.h"
+
+CSplash::CSplash() {
+	Init();
+}
+
+CSplash::CSplash(LPCTSTR name, unsigned transparentColor) {
+	Init();
+	SetBitmap(name);
+	SetTransparentColor(transparentColor);
+}
+
+CSplash::~CSplash() {
+	FreeResources();
+}
+
+void CSplash::Init() {
+	GetModuleHandle(TEXT("USER32.DLL"));
+}
+
+void CSplash::OnPaint(HWND hWnd) const {
+	RECT rect;
+	PAINTSTRUCT paint;
+
+	HDC hdc = BeginPaint(hWnd, &paint);
+	if (this->m_bitmap) {
+		GetClientRect(m_hWnd, &rect);
+		HDC hdcSrc = CreateCompatibleDC(hdc);
+		HGDIOBJ prevBitmap = SelectObject(hdcSrc, this->m_bitmap);
+		BitBlt(hdc, 0, 0, this->m_bitmapW, this->m_bitmapH, hdcSrc, 0, 0, SRCCOPY);
+		SelectObject(hdcSrc, prevBitmap);
+		DeleteDC(hdcSrc);
+	} else {
+		SetBkMode(hdc, 1);
+		TextOutA(hdc, this->m_textX, this->m_textY, this->m_textStr, strlen(this->m_textStr));
+	}
+	EndPaint(hWnd, &paint);
+}
+
+void CSplash::SetText(char* str) {
+	strcpy_s(this->m_textStr, sizeof(this->m_textStr), str);
+	OnPaint(this->m_hWnd);
+	InvalidateRect(this->m_hWnd, nullptr, FALSE);
+}
+
+int CSplash::CloseSplash() {
+	ShowWindow(this->m_hWnd, SW_HIDE);
+	if (!this->m_hWnd) {
+		return 0;
+	}
+	DestroyWindow(this->m_hWnd);
+	this->m_hWnd = nullptr;
+	UnregisterClass(this->m_className, GetModuleHandleA(0));
+	return 1;
+}
+
+void CSplash::HideSplash() const {
+	ShowWindow(this->m_hWnd, SW_HIDE);
+}
+
+void CSplash::FreeResources() {
+	if (this->m_bitmap) {
+		DeleteObject(this->m_bitmap);
+	}
+	this->m_bitmap = nullptr;
+}
+
+bool CSplash::MakeTransparent() const {
+	if (this->m_hWnd && this->m_setLayeredWindowAttributesProc && this->m_colorRef) {
+		LONG exStyle = GetWindowLongA(this->m_hWnd, GWL_EXSTYLE);
+		SetWindowLongA(this->m_hWnd, GWL_EXSTYLE, exStyle | WS_EX_LAYERED);
+		this->m_setLayeredWindowAttributesProc(this->m_hWnd, this->m_colorRef, 0, 1);
+	}
+	return true;
+}
+
+LRESULT CSplash::WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) const {
+	if (msg == WM_PAINT) {
+		OnPaint(hWnd);
+		return 0;
+	}
+	return DefWindowProcA(hWnd, msg, wParam, lParam);
+}
+
+unsigned int CSplash::SetBitmap(HBITMAP h) {
+	BITMAP bmi;
+	if (this->m_bitmap) {
+		DeleteObject(this->m_bitmap);
+	}
+	this->m_bitmap = nullptr;
+	if (!h) {
+		return 1;
+	}
+	this->m_bitmap = h;
+	if (GetObjectA(h, sizeof(bmi), &bmi)) {
+		this->m_bitmapW = bmi.bmWidth;
+		this->m_bitmapH = bmi.bmHeight;
+		return 1;
+	}
+	if (this->m_bitmap) {
+		DeleteObject(this->m_bitmap);
+	}
+	this->m_bitmap = nullptr;
+	return 0;
+}
+
+bool CSplash::SetTransparentColor(unsigned int colorRef) {
+	this->m_colorRef = colorRef;
+	if (this->m_hWnd && this->m_setLayeredWindowAttributesProc && this->m_colorRef) {
+		LONG exStyle = GetWindowLongA(this->m_hWnd, GWL_EXSTYLE);
+		SetWindowLongA(this->m_hWnd, GWL_EXSTYLE, exStyle | WS_EX_LAYERED);
+		this->m_setLayeredWindowAttributesProc(this->m_hWnd, this->m_colorRef, 0, 1);
+	}
+	return true;
+}
+
+LRESULT CALLBACK CSplash::WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	static CSplash* splash;
+
+	if (msg == WM_CREATE) {
+		splash = *reinterpret_cast<CSplash**>(lParam);
+	}
+	if (!splash || msg != WM_PAINT) {
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	splash->OnPaint(hWnd);
+	return 0;
+}
+
+HWND CSplash::RegAndCreateWindow() {
+	WNDCLASSEX wndClass;
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.style = 0x3000;
+	wndClass.lpfnWndProc = WndProc;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 30;
+	wndClass.hInstance = GetModuleHandle(nullptr);
+	wndClass.hIcon = nullptr;
+	wndClass.hCursor = LoadCursorA(0, (LPCSTR)0x7F02);
+	wndClass.hbrBackground = static_cast<HBRUSH>(GetStockObject(1));
+	wndClass.lpszMenuName = nullptr;
+	wndClass.lpszClassName = this->m_className;
+	wndClass.hIconSm = nullptr;
+
+	if (!RegisterClassEx(&wndClass)) {
+		return nullptr;
+	}
+
+	int screenWidth = GetSystemMetrics(SM_CXFULLSCREEN);
+	int screenHeight = GetSystemMetrics(SM_CYFULLSCREEN);
+	if (!this->m_bitmapH && !this->m_bitmapW) {
+		this->m_bitmapW = 480;
+		this->m_bitmapH = 360;
+	}
+
+	HWND hWnd = CreateWindowEx(
+		WS_EX_TOOLWINDOW,
+		this->m_className,
+		nullptr,
+		WS_POPUP,
+		static_cast<unsigned int>(screenWidth - this->m_bitmapW) / 2,
+		static_cast<unsigned int>(screenHeight - this->m_bitmapH) / 2,
+		this->m_bitmapW,
+		this->m_bitmapH,
+		nullptr,
+		nullptr,
+		nullptr,
+		this);
+	this->m_hWnd = hWnd;
+	if (hWnd) {
+		MakeTransparent();
+		ShowWindow(this->m_hWnd, 5);
+		UpdateWindow(this->m_hWnd);
+	}
+	return this->m_hWnd;
+}
+
+void CSplash::ShowSplash() {
+	ShowWindow(this->m_hWnd, SW_HIDE);
+	if (this->m_hWnd) {
+		DestroyWindow(this->m_hWnd);
+		this->m_hWnd = nullptr;
+		UnregisterClass(this->m_className, GetModuleHandle(nullptr));
+	}
+	RegAndCreateWindow();
+	strcpy_s(this->m_textStr, sizeof(this->m_textStr), "");
+	OnPaint(this->m_hWnd);
+	InvalidateRect(this->m_hWnd, nullptr, 0);
+	ShowWindow(this->m_hWnd, 5);
+}
+
+unsigned int CSplash::SetBitmap(LPCTSTR name) {
+	return SetBitmap(static_cast<HBITMAP>(LoadImage(nullptr, name, 0, 0, 0, 0x10)));
+}
+
+WPARAM CSplash::DoLoop() {
+	MSG msg;
+	if (!this->m_hWnd) {
+		ShowSplash();
+	}
+	while (GetMessageA(&msg, 0, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessageA(&msg);
+	}
+	return msg.wParam;
+}
---
+++ game/splash.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include <windows.h>
+
+#include "baseobject.h"
+#include "singleton.h"
+
+typedef BOOL(__stdcall* SetLayeredWindowAttributesProc)(HWND, COLORREF, BYTE, DWORD);
+
+class CSplash : public BaseObject, public WSingleton<CSplash> {
+public:
+	CSplash();
+	CSplash(LPCTSTR name, unsigned int transparentColor);
+	~CSplash();
+
+	void OnPaint(HWND hWnd) const;
+	void SetText(char* str);
+	int CloseSplash();
+	void HideSplash() const;
+	void FreeResources();
+	bool MakeTransparent() const;
+	HRESULT WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) const;
+	unsigned int SetBitmap(HBITMAP h);
+	bool SetTransparentColor(unsigned int colorRef);
+	HWND RegAndCreateWindow();
+	void ShowSplash();
+	unsigned int SetBitmap(LPCTSTR name);
+	WPARAM DoLoop();
+	
+private:
+	void Init();
+	static LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+	unsigned int m_colorRef;
+	int m_bitmapW;
+	int m_bitmapH;
+	HBITMAP m_bitmap;
+	HWND m_hWnd;
+	const TCHAR* m_className;
+	char m_textStr[128];
+	int m_textX;
+	int m_textY;
+	SetLayeredWindowAttributesProc m_setLayeredWindowAttributesProc;
+};
