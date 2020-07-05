2098fc3e4c38743f6f8bb2b44b8d473529603efa
John Chadwick <john@jchw.io>
2020-07-05 00:46:31 -0700

    Begin ProjectG.exe.

---
+++ game/main.cpp
@@ -0,0 +1,10 @@
+#include <windows.h>
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow) {
+	UNREFERENCED_PARAMETER(hInstance);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(nCmdShow);
+
+    return 0;
+}
