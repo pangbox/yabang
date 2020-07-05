9264207c544a34aa09c3670e13eb200c0da07c33
John Chadwick <john@jchw.io>
2020-07-05 13:05:18 -0700

    Add BaseObject implementation.

---
+++ game/baseobject.cpp
@@ -0,0 +1,14 @@
+﻿#include "baseobject.h"
+
+int BaseObject::m_nextId = 0;
+int BaseObject::m_count = 0;
+ConstructFn BaseObject::m_constructFn = nullptr;
+
+BaseObject::BaseObject() {
+	this->m_ref = 0;
+	this->m_id = m_nextId++;
+	++m_count;
+	if (m_constructFn) {
+		m_constructFn(this);
+	}
+}
---
+++ game/baseobject.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <string>
+
+class BaseObject;
+
+typedef void (__fastcall* ConstructFn)(BaseObject*);
+
+class BaseObject {
+public:
+	BaseObject();
+
+protected:
+	static int m_nextId;
+	static int m_count;
+	static ConstructFn m_constructFn;
+	
+	int m_id = 0;
+	int m_ref;
+	std::string m_leakHint = "";
+};
