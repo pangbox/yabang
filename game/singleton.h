993db04944d9ba7f4875b77dcd773a18873f4011
John Chadwick <john@jchw.io>
2020-07-05 13:04:23 -0700

    Add WSingleton implementation.

---
+++ game/singleton.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+template <typename T>
+class WSingleton {
+public:
+	static T* Instance() {
+        return m_pInstance;
+	}
+	WSingleton() {
+		m_pInstance = static_cast<T*>(this);
+	}
+	~WSingleton() {
+		m_pInstance = nullptr;
+	}
+    WSingleton(const WSingleton&) = delete;
+    WSingleton& operator= (const WSingleton&) = delete;
+	WSingleton(WSingleton&&) = delete;
+	WSingleton& operator= (WSingleton&&) = delete;
+
+private:
+    inline static T* m_pInstance = nullptr;
+};
