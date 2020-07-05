bd97b5e9f0a134ba017213e137bec61888d14375
John Chadwick <john@jchw.io>
2020-07-05 02:18:26 -0700

    Entrypoint initializes COM, common controls, ATL.

--- game/main.cpp
+++ game/main.cpp
@@ -1,4 +1,7 @@
 #include <windows.h>
+#include <atlbase.h>
+#include <atlwin.h>
+#include <commctrl.h>
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow) {
 	UNREFERENCED_PARAMETER(hInstance);
@@ -6,5 +9,14 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
 	UNREFERENCED_PARAMETER(lpCmdLine);
 	UNREFERENCED_PARAMETER(nCmdShow);
 
-    return 0;
+	CoInitialize(nullptr);
+
+	INITCOMMONCONTROLSEX initCommonControlsEx{};
+	initCommonControlsEx.dwSize = sizeof(initCommonControlsEx);
+	initCommonControlsEx.dwICC = ICC_BAR_CLASSES | ICC_COOL_CLASSES;
+	InitCommonControlsEx(&initCommonControlsEx);
+
+	AtlAxWinInit();
+	
+	return 0;
 }
