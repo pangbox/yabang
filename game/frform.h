42c118874dedd20b2c1671331d13d3c81684fc70
John Chadwick <john@jchw.io>
2020-07-06 12:28:48 -0700

    Add FrForm class.

---
+++ game/frform.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include "frwnd.h"
+
+class FrFrame;
+class FrStatic;
+class FrEdit;
+
+enum eFormRet {
+	FrNONE = 0x0,
+	FrOK = 0x1,
+	FrCANCEL = 0x2,
+	FrYES = 0x3,
+	FrNO = 0x4,
+};
+
+class FrForm : public FrWnd {
+public:
+
+private:
+	bool m_canDrag;
+	bool m_bCaptionDown;
+	bool m_bResizeBtnDown;
+	unsigned int m_flag;
+	eFormRet m_retCode;
+	bool (FrCmdTarget::* m_pFnResult)(int, FrForm*);
+	FrCmdTarget* m_pCmdDest;
+	FrFrame* m_pBaseFrm;
+	FrStatic* m_pMsgStatic;
+	FrEdit* m_pMsgEdit;
+	float m_timeLimit;
+	float m_dt;
+	eFormRet m_defaultRetCode;
+};
