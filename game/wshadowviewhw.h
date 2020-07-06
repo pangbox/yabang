ad1d98f9b5a475cf9bdb264cbbfb3c37fcfdd08c
John Chadwick <john@jchw.io>
2020-07-06 03:41:19 -0700

    Begin WShadowViewHw class.

---
+++ game/wshadowviewhw.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "wvideo.h"
+#include "wshadowview.h"
+#include "wtextureview.h"
+
+class WShadowViewHw : public WShadowView {
+public:
+	WRenderToTextureParam m_orgTexParam;
+	WTextureView* m_filteredTexView;
+	bool m_r2tBegun;
+};
