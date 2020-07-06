9e4319b598430819a75bc03a2e7da6cb600e2afb
John Chadwick <john@jchw.io>
2020-07-06 03:37:37 -0700

    Begin FrWnd class.

---
+++ game/frwnd.cpp
@@ -0,0 +1,1 @@
+﻿#include "frwnd.h"
---
+++ game/frwnd.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include <list>
+#include <string>
+
+#include "frcmdtarget.h"
+#include "wflags.h"
+#include "wmath.h"
+
+class FrWndManager;
+class FrScrollBar;
+
+class FrWnd {
+private:
+	struct sToolTipData {
+		int bFixWnd;
+		WPoint posFixWnd;
+		unsigned int style;
+	};
+
+	WFlags m_nFlags;
+	FrWnd* m_pParentWnd;
+	FrCmdTarget* m_pOwner;
+	std::list<FrWnd*> m_childList;
+	WRect m_rect;
+	WFlags m_dwStyle;
+	std::string m_wndText;
+	std::string m_wndName;
+	float m_wndAlpha;
+	float m_wndAlpha2;
+	unsigned int m_nRefID;
+	FrWndManager* m_pWndManager;
+	FrScrollBar* m_pScrBar;
+	float m_fadeTime;
+	WRect m_iconRect;
+	const char* m_szPushSound;
+	float m_dblClickTimeout;
+	bool m_dblClicked;
+	WPoint m_dblClickPos;
+	bool m_hoverOn;
+	float m_hoverTime;
+	float m_accHoverTime;
+	std::basic_string<char, std::char_traits<char>, std::allocator<char> > m_wndToolTip;
+	sToolTipData* m_pToolTipData;
+};
