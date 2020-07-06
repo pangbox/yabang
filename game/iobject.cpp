2130c85b58db559fa680e08c9eede856523d0a6c
John Chadwick <john@jchw.io>
2020-07-06 03:34:58 -0700

    Implement IObject.

---
+++ game/iobject.cpp
@@ -0,0 +1,55 @@
+﻿#include "iobject.h"
+
+WRTTI IObject::m_RTTI = WRTTI("IObject", nullptr);
+
+WRTTI::WRTTI(const char* pName, WRTTI* pBaseRtti): m_pName(pName),
+                                                   m_pBaseRTTI(pBaseRtti) {}
+
+WRTTI* WRTTI::GetBaseRTTI() const {
+	return this->m_pBaseRTTI;
+}
+
+const char* WRTTI::GetName() const {
+	return this->m_pName;
+}
+
+IObject::IObject() = default;
+
+IObject::~IObject() = default;
+
+bool IObject::IsKindOf(const WRTTI* pOther) {
+    WRTTI* pSelf = GetRTTI();
+    if (!pSelf) {
+        return false;
+    }
+    while (pSelf != pOther) {
+        pSelf = pSelf->m_pBaseRTTI;
+        if (!pSelf) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool IObject::IsExactKindOf(const WRTTI* pOther) {
+    return GetRTTI() == pOther;
+}
+
+IObject* IObject::DynamicCast(const WRTTI* pOther) {
+    WRTTI* pSelf = GetRTTI();
+    if (!pSelf) {
+        return nullptr;
+    }
+    while (pSelf != pOther)
+    {
+        pSelf = pSelf->m_pBaseRTTI;
+        if (!pSelf) {
+            return nullptr;
+        }
+    }
+    return this;
+}
+
+WRTTI* IObject::GetRTTI() {
+    return &m_RTTI;
+}
---
+++ game/iobject.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+class WRTTI {
+	friend class IObject;
+	
+public:
+	WRTTI(const char* pName, WRTTI* pBaseRtti);
+	WRTTI* GetBaseRTTI() const;
+	const char* GetName() const;
+
+private:
+	const char* m_pName;
+	WRTTI* m_pBaseRTTI;
+};
+
+
+class IObject {
+public:
+	IObject();
+	virtual ~IObject();
+
+	bool IsKindOf(const WRTTI* pOther);
+	bool IsExactKindOf(const WRTTI* pOther);
+	IObject* DynamicCast(const WRTTI* pOther);
+	virtual WRTTI* GetRTTI();
+
+private:
+	static WRTTI m_RTTI;
+};
