d16a2cfd0d3b6c0d88ecae94433e543fabf10112
John Chadwick <john@jchw.io>
2020-07-06 12:28:59 -0700

    Add FrCursor class.

---
+++ game/frcursor.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+class FrCursor {
+public:
+	enum eCursor {
+		UNSELECT = 0x0,
+		NORMAL = 0x0,
+		ROLL_OVER = 0x1,
+		SELECT = 0x2,
+		WRONG = 0x3,
+		UP = 0x4,
+		DOWN = 0x5,
+		ZOOM = 0x6,
+		INFO = 0x7,
+	};
+
+private:
+	int m_cursorIndex;
+	bool m_bShow;
+};
