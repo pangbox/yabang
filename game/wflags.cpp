9dfd7881c60629e1697592543ee9f072bf0e0d31
John Chadwick <john@jchw.io>
2020-07-06 03:36:36 -0700

    Implement WFlags class.

---
+++ game/wflags.cpp
@@ -0,0 +1,38 @@
+﻿#include "wflags.h"
+
+WFlags::WFlags() = default;
+
+WFlags::WFlags(uint32_t& flag)
+	: m_flag(flag) {}
+
+void WFlags::Set(uint32_t flag) {
+	this->m_flag = flag;
+}
+
+void WFlags::Reset() {
+	this->m_flag = 0;
+}
+
+void WFlags::Disable(uint32_t flag) {
+	this->m_flag &= ~flag;
+}
+
+void WFlags::Turn(uint32_t flag, bool on) {
+	if (on) {
+		this->m_flag |= flag;
+	} else {
+		this->m_flag &= ~flag;
+	}
+}
+
+bool WFlags::GetFlag(uint32_t flag) {
+	return (flag & this->m_flag) != 0;
+}
+
+void WFlags::Enable(uint32_t flag) {
+	this->m_flag |= flag;
+}
+
+WFlags::operator unsigned() const {
+	return m_flag;
+}
---
+++ game/wflags.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <cstdint>
+
+class WFlags {
+public:
+	WFlags();
+	explicit WFlags(uint32_t& flag);
+
+	void Set(uint32_t flag);
+	void Reset();
+	void Disable(uint32_t flag);
+	void Turn(uint32_t flag, bool on);
+	bool GetFlag(uint32_t flag);
+	void Enable(uint32_t flag);
+
+	operator uint32_t() const;
+
+private:
+	uint32_t m_flag = 0;
+};
