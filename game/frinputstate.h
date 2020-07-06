7dc6984a73ee1368ba7284279794db603a1d423e
John Chadwick <john@jchw.io>
2020-07-06 03:39:38 -0700

    Begin FrInputState struct.

---
+++ game/frinputstate.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "wmath.h"
+
+class FrWnd;
+class FrScrollBar;
+class CChatMsg;
+
+struct FrInputState {
+	unsigned int mouse{};
+	bool hoverChecked{};
+	WPoint mousePos;
+	WPoint oldMousePos;
+	float wheelDelta{};
+	FrWnd* keyFocused{};
+	FrScrollBar* wheelFocused{};
+	CChatMsg* im{};
+};
