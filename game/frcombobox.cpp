9e559c00f36f3d2d0218fd3df80ee77ebeeb7417
John Chadwick <john@jchw.io>
2020-07-06 03:37:58 -0700

    Begin FrComboBox class.

---
+++ game/frcombobox.cpp
@@ -0,0 +1,1 @@
+﻿#include "frcombobox.h"
---
+++ game/frcombobox.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "fredit.h"
+#include "frguiitem.h"
+
+class FrButton;
+class FrListBox;
+
+class FrComboBox : public FrEdit {
+public:
+
+private:
+	int m_maxListNum;
+	std::list<FrLine*, std::allocator<FrLine*> > m_comboList;
+	std::list<FrLine*, std::allocator<FrLine*> >::iterator m_selected;
+	FrButton* m_pButton;
+	FrGuiItem m_buttonInfo;
+	FrListBox* m_pListBox;
+	FrGuiItem m_listInfo;
+	unsigned int m_selectColor;
+};
