a7da89c6a0585bc4823d52c52b2d09eea7ad2a35
John Chadwick <john@jchw.io>
2020-07-06 03:42:04 -0700

    Begin WOverlay class.

---
+++ game/woverlay.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "wmath.h"
+#include "wresource.h"
+
+class WOverlay : public WResource {
+public:
+
+private:
+	int m_texHandle;
+	unsigned int m_texWidth;
+	unsigned int m_texHeight;
+	unsigned int m_devTexWidth;
+	unsigned int m_devTexHeight;
+	WRect m_clipArea;
+	bool m_clipFlag;
+	int m_coordMode;
+};
