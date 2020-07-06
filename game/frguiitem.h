7e13c741862b1a6e0de4e48356defb988329e992
John Chadwick <john@jchw.io>
2020-07-06 03:36:55 -0700

    Begin FrGuiItem class.

---
+++ game/frguiitem.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include <map>
+#include <string>
+
+#include "rectangle.h"
+
+enum enumGuiType {
+	GI_NONE,
+	GI_FORM,
+	GI_STATIC,
+	GI_TEXTBUTTON,
+	GI_EDIT,
+	GI_COMBOBOX,
+	GI_COMBOCTLEX,
+	GI_BUTTON,
+	GI_FRAME,
+	GI_RESOURCE,
+	GI_BITMAP,
+	GI_AREA,
+	GI_LISTBOX,
+	GI_GAUGEBAR,
+	GI_GAUGEBAREX,
+	GI_GAUGEBARIMAGE,
+	GI_VIEWER,
+	GI_CONTEXTMENU,
+	GI_TABBUTTON,
+	GI_GROUPBOX,
+	GI_MACROITEM,
+	GI_LAST,
+};
+
+class FrGuiItem {
+public:
+	enumGuiType m_type;
+	std::string m_resource;
+	std::string m_caption;
+	std::string m_name;
+	std::map<std::string, std::string> m_param;
+	unsigned int m_flag;
+	Rectangle<short> m_rect;
+};
