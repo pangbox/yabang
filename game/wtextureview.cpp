f48df84c76910e5587fbc8b41536d5d36db65eab
John Chadwick <john@jchw.io>
2020-07-06 03:41:38 -0700

    Begin WTextureView class.

---
+++ game/wtextureview.cpp
@@ -0,0 +1,1 @@
+﻿#include "wtextureview.h"
---
+++ game/wtextureview.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "wvideo.h"
+#include "wview.h"
+
+class WTextureView : public WView {
+public:
+
+private:
+	struct TextureParam {
+		struct RtTexInfo {
+			unsigned int m_texStyle;
+			WRenderToTextureSizeInfo m_sizeInfo;
+			bool m_needToClear;
+			unsigned int m_clearClr;
+		};
+
+		struct DepthSurfInfo {
+			WRenderToTextureParam::DepthSurfInfo::SurfaceUsage m_surfUsage;
+			WRenderToTextureSizeInfo m_sizeInfo;
+			bool m_needToClear;
+			float m_clearZ;
+		};
+
+		unsigned int m_numRts;
+		RtTexInfo m_rtTexInfo[2];
+		DepthSurfInfo m_depthSurfInfo;
+	};
+	
+	TextureParam m_texParam;
+	WRenderToTextureParam m_r2tParam;
+	bool m_r2tBegun;
+};
