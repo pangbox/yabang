78ac79325feb65c590e9ee67c9d9981df46d840a
John Chadwick <john@jchw.io>
2020-07-06 03:33:51 -0700

    Begin Rectangle struct.

---
+++ game/rectangle.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "coordinates.h"
+
+template<typename T>
+struct Rectangle {
+	union {
+		Coordinates<T> tl;
+		struct { T left, top; };
+	};
+	union {
+		Coordinates<T> br;
+		struct { T right, bottom; };
+	};
+};
