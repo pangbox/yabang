3cdf96635e082047723e16f352babd16f5a6bf4a
John Chadwick <john@jchw.io>
2020-07-06 03:39:07 -0700

    Begin WResrcCache class.

---
+++ game/wresrccache.cpp
@@ -0,0 +1,1 @@
+﻿#include "wresrccache.h"
---
+++ game/wresrccache.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "wlist.h"
+
+template <typename T>
+class WResrcCache {
+public:
+	struct w_origin {
+		T resource;
+		int count;
+		int checkflag;
+		char name[1];
+	};
+
+	struct w_cache {
+		w_origin* origin;
+		T resource;
+	};
+
+	WList<w_cache*> resrcList;
+	WList<w_origin*> originList;
+};
