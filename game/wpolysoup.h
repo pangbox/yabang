771e9f9d65b46c1019a05fd230a4ff3490356e91
John Chadwick <john@jchw.io>
2020-07-07 22:22:02 -0700

    Add skeleton for WPolySoup.

---
+++ game/wpolysoup.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+class WPolySoup {
+public:
+	struct sModel {};
+
+	struct sCamera {};
+
+	struct sTriangle {};
+
+	struct sTexture {};
+
+	struct sBgFx {};
+
+	struct sCupVtx {};
+
+	struct sHoleTri {};
+
+	struct sHoleVtx {};
+
+	struct sArea {};
+
+	struct sQuad {};
+
+	struct sPoint {};
+
+	struct sHoleMesh {};
+
+	class CStaticPetGrp {};
+
+	class SmInFragList {};
+};
