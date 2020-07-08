48a0845b7ddb84a1f73ce350fb3ed2051e0fae2c
John Chadwick <john@jchw.io>
2020-07-07 22:21:43 -0700

    Add sPakInfo structure.

---
+++ game/spakinfo.cpp
@@ -0,0 +1,1 @@
+﻿#include "spakinfo.h"
---
+++ game/spakinfo.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <cstdint>
+
+struct sPakinfo {
+	uint8_t type;
+	uint16_t next;
+	uint32_t offset;
+	uint32_t packedsize;
+	uint32_t size;
+	char filename[1];
+};
