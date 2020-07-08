d1843d4dc1f5e892d592c34ce48d7f0d5ddfdec7
John Chadwick <john@jchw.io>
2020-07-07 22:21:34 -0700

    Add sPak structure.

---
+++ game/spak.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include <cstdint>
+
+class cFile;
+struct sPakinfo;
+
+struct sPak {
+	int num;
+	cFile* fp;
+	int offset;
+	uint16_t hash[256];
+	void* lock;
+	sPakinfo** list;
+	sPak* next;
+};
