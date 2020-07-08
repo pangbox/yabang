187f025b557ce594c99526ac3eca9cdd32084184
John Chadwick <john@jchw.io>
2020-07-07 22:20:33 -0700

    Implement cFileDefault.

---
+++ game/cfiledefault.cpp
@@ -0,0 +1,27 @@
+﻿#include "cfiledefault.h"
+
+#include <cstdint>
+#include <io.h>
+
+cFileDefault::cFileDefault(int fd) : m_fd(fd) {}
+
+cFileDefault::~cFileDefault() {
+	_close(m_fd);
+}
+
+int cFileDefault::Read(void* data, int len) {
+	return _read(m_fd, data, len);
+}
+
+int cFileDefault::GetByte() {
+	uint8_t code = 0;
+	return this->Read(&code, 1) ? static_cast<uint8_t>(code) : -1;
+}
+
+int cFileDefault::Tell() {
+	return _tell(m_fd);
+}
+
+void cFileDefault::Seek(int pos, int origin) {
+	_lseek(m_fd, pos, origin);
+}
---
+++ game/cfiledefault.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "cfile.h"
+
+class cFileDefault final : public cFile {
+public:
+	explicit cFileDefault(int fd);
+	~cFileDefault() override;
+	int Read(void* data, int len) override;
+	int GetByte() override;
+	int Tell() override;
+	void Seek(int pos, int origin) override;
+
+private:
+	int m_fd;
+};
