7fde9824f3d3bcfcffaf16597534d836e850d2c3
John Chadwick <john@jchw.io>
2020-07-17 23:32:23 -0700

    Add FrEmoticon stub.

---
+++ game/fremoticon.cpp
@@ -0,0 +1,1 @@
+﻿#include "fremoticon.h"
---
+++ game/fremoticon.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <map>
+#include <string>
+#include <vector>
+
+class WOverlay;
+class FrWndManager;
+
+class FrEmoticon {
+public:
+	struct sEmoticon {
+		std::string Name;
+		std::string Alias[2];
+		WOverlay* Overlay;
+		std::vector<int> Frames;
+	};
+
+
+protected:
+	int m_Fps;
+	std::vector<sEmoticon*, std::allocator<sEmoticon*>> m_Emoticons;
+	std::map<std::string, WOverlay*> m_ResMap;
+	bool m_EnableAnim;
+	int m_AnimStart;
+	WOverlay* m_overlay;
+	int m_selWidth;
+	int m_selHeight;
+	int m_selXNum;
+	int m_selYNum;
+	int m_selNum;
+	FrWndManager* m_pManager;
+};
