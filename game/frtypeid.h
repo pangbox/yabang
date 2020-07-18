36d8cb6fd03189094d77a0f9ad5d028845636b0d
John Chadwick <john@jchw.io>
2020-07-17 21:15:02 -0700

    Add FrTypeID class.

---
+++ game/frtypeid.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <map>
+#include <string>
+
+template <typename T>
+class FrTypeID {
+public:
+	T operator[](const std::string& typeId) const {
+		return this->m_map.at(typeId);
+	}
+
+protected:
+	std::map<std::string, T> m_map;
+};
