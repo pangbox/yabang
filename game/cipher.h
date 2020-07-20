ce0f6ab2f7c61eaa7393e5135b01cc2987823b57
John Chadwick <john@jchw.io>
2020-07-20 00:12:41 -0700

    Add KR XTEA key.

--- game/cipher.h
+++ game/cipher.h
@@ -1,4 +1,6 @@
 ﻿#pragma once
 #include <cstdint>
 
+constexpr static uint32_t g_xteaKeyKr[] = { 0x485B576, 0x5148E02, 0x5141D96, 0x28FA9D6 };
+
 void Decipher(const uint32_t* v, uint32_t* w, const uint32_t* k);
