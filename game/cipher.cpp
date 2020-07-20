4c08cf0f8f66f191cf9cba228ccd89f29b88642a
John Chadwick <john@jchw.io>
2020-07-19 23:32:00 -0700

    Implement XTEA cipher.

---
+++ game/cipher.cpp
@@ -0,0 +1,12 @@
+﻿#include "cipher.h"
+
+void Decipher(const uint32_t* v, uint32_t* w, const uint32_t* k) {
+	uint32_t sum = 0xE3779B90;
+	w[0] = v[0];
+	w[1] = v[1];
+	for (int i = 0; i < 16; i++) {
+		w[1] -= (w[0] << 4 ^ w[0] >> 5) + w[0] ^ sum + k[sum >> 11 & 3];
+		sum += 0x61C88647;
+		w[0] -= (w[1] << 4 ^ w[1] >> 5) + w[1] ^ sum + k[sum & 3];
+	}
+}
---
+++ game/cipher.h
@@ -0,0 +1,4 @@
+﻿#pragma once
+#include <cstdint>
+
+void Decipher(const uint32_t* v, uint32_t* w, const uint32_t* k);
