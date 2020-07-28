aca93392163907d6f79b9b09696d229c566946a9
John Chadwick <john@jchw.io>
2020-07-28 00:22:29 -0700

    CInputManager stub.

---
+++ game/cinputmanager.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <cstdint>
+
+#include "wlist.h"
+#include "wmath.h"
+
+class CInputManager {
+public:
+	struct sButton {};
+
+protected:
+	// TODO: Some state is missing still.
+	WVector m_mouseDelta;
+	WVector m_mousePoint;
+	bool m_bFixPointer;
+	sButton m_buttons[3];
+	float m_fSensitivity;
+	bool m_bActive;
+	bool m_bSwapMouseButton;
+};
